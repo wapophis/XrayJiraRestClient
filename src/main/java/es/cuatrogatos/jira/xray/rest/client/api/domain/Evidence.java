eb4bcfb8e41c14707a518c6c0a79945962e86357
wapophis <atlialogic@gmail.com>
2016-08-22 12:37:04 +0200

    Added : * Attachments at TestStep. * Examples at TestRun. Updated: * TestRunJsonParserTest

--- src/main/java/es/cuatrogatos/jira/xray/rest/client/api/domain/Evidence.java
+++ src/main/java/es/cuatrogatos/jira/xray/rest/client/api/domain/Evidence.java
@@ -22,4 +22,52 @@ public class Evidence {
         this.author=author;
         this.fileURL=fileURL;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public URI getFileURL() {
+        return fileURL;
+    }
+
+    public void setFileURL(URI fileURL) {
+        this.fileURL = fileURL;
+    }
 };
--- src/test/java/es/cuatrogatos/jira/xray/rest/client/core/internal/json/TestRunJsonParserTest.java
+++ src/test/java/es/cuatrogatos/jira/xray/rest/client/core/internal/json/TestRunJsonParserTest.java
@@ -1,6 +1,8 @@
 package es.cuatrogatos.jira.xray.rest.client.core.internal.json;
 
-import es.cuatrogatos.jira.xray.rest.client.api.domain.TestRun;
+import es.cuatrogatos.jira.xray.rest.client.api.domain.*;
+import es.cuatrogatos.jira.xray.rest.client.core.internal.json.util.XrayJiraDateFormatter;
+import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
@@ -8,6 +10,7 @@ import org.junit.Test;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.logging.Logger;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -31,10 +34,12 @@ public class TestRunJsonParserTest {
     private String KEY_DEFECTS="defects";
     private String KEY_EVIDENCES="evidences";
 
+    private final XrayJiraDateFormatter xrayJiraDateFormatter=new XrayJiraDateFormatter();
 
 
 
-    private String data="{\n" +
+
+    private String[] data={"{\n" +
             "  \"id\": 371,\n" +
             "  \"status\": \"TODO\",\n" +
             "  \"executedBy\": \"admin\",\n" +
@@ -123,7 +128,64 @@ public class TestRunJsonParserTest {
             "      \"statusDescription\": \"The test run has passed\"\n" +
             "    }\n" +
             "  ]\n" +
-            "}";
+            "}",
+            "{" +
+                    "  \"id\": 2022,\n" +
+                    "  \"status\": \"TODO\",\n" +
+                    "  \"startedOn\": \"sábado 7:44 PM\",\n" +
+                    "  \"defects\": [],\n" +
+                    "  \"evidences\": [],\n" +
+                    "  \"steps\": [\n" +
+                    "    {\n" +
+                    "      \"id\": 7388,\n" +
+                    "      \"index\": 1,\n" +
+                    "      \"step\": {\n" +
+                    "        \"raw\": \"Paso 1\",\n" +
+                    "        \"rendered\": \"<p>Paso 1</p>\"\n" +
+                    "      },\n" +
+                    "      \"data\": {\n" +
+                    "        \"raw\": \"DATA 1\",\n" +
+                    "        \"rendered\": \"<p>DATA 1</p>\"\n" +
+                    "      },\n" +
+                    "      \"result\": {\n" +
+                    "        \"raw\": \"EXPT 1\\n\",\n" +
+                    "        \"rendered\": \"<p>EXPT 1</p>\"\n" +
+                    "      },\n" +
+                    "      \"attachments\": [],\n" +
+                    "      \"status\": \"TODO\",\n" +
+                    "      \"comment\": {\n" +
+                    "        \"rendered\": \"\"\n" +
+                    "      },\n" +
+                    "      \"defects\": [],\n" +
+                    "      \"evidences\": []\n" +
+                    "    },\n" +
+                    "    {\n" +
+                    "      \"id\": 7389,\n" +
+                    "      \"index\": 2,\n" +
+                    "      \"step\": {\n" +
+                    "        \"raw\": \"PASO 2\",\n" +
+                    "        \"rendered\": \"<p>PASO 2</p>\"\n" +
+                    "      },\n" +
+                    "      \"data\": {\n" +
+                    "        \"raw\": \"DATA 2\",\n" +
+                    "        \"rendered\": \"<p>DATA 2</p>\"\n" +
+                    "      },\n" +
+                    "      \"result\": {\n" +
+                    "        \"raw\": \"EXPT 2\\n\",\n" +
+                    "        \"rendered\": \"<p>EXPT 2</p>\"\n" +
+                    "      },\n" +
+                    "      \"attachments\": [],\n" +
+                    "      \"status\": \"TODO\",\n" +
+                    "      \"comment\": {\n" +
+                    "        \"rendered\": \"\"\n" +
+                    "      },\n" +
+                    "      \"defects\": [],\n" +
+                    "      \"evidences\": []\n" +
+                    "    }\n" +
+                    "  ]\n" +
+                    "}"
+
+    };
 
     private String[] parseable_dates={"22/Jun/15 11:25 AM","Viernes 1:19 PM","Hoy 7:29 PM"};
 
@@ -142,20 +204,70 @@ public class TestRunJsonParserTest {
 
     @Test
     public void testParse() throws Exception {
-        JSONObject jsonObject=new JSONObject(data);
-        TestRun testRun=parser.parse(jsonObject);
-        assertEquals(testRun.getId().longValue(),jsonObject.getLong("id"));
-        assertEquals(testRun.getKey()," THERE IS NO KEY FOR TEST RUN AT X-RAY DIRECT REST API");
-        assertEquals(testRun.getStatus().name(),jsonObject.getString("status"));
-        assertEquals(new SimpleDateFormat("dd/MMM/yy hh:mm aa").format(testRun.getStartedOn()).toLowerCase(),jsonObject.getString("startedOn").toLowerCase());
-        assertEquals(testRun.getExecutedBy(),jsonObject.getString("executedBy"));
-        Iterator it=testRun.getDefects().iterator();
-        int count=0;
-        while(it.hasNext()){
-            count++;
-            it.next();
+        for(int i=0;i<data.length;i++) {
+            JSONObject jsonObject = new JSONObject(data[i]);
+
+            TestRun testRun = parser.parse(jsonObject);
+            assertEquals(testRun.getId().longValue(), jsonObject.getLong("id"));
+            assertEquals(testRun.getKey(), " THERE IS NO KEY FOR TEST RUN AT X-RAY DIRECT REST API");
+            try{
+                assertEquals(testRun.getStatus().name(), jsonObject.getString("status"));
+            }catch(JSONException nE){
+            Logger.getAnonymousLogger().severe("There is no STATUS field in this test data["+i+"] bank.");
+            }
+            assertEquals(testRun.getStartedOn(), xrayJiraDateFormatter.parse(jsonObject.getString("startedOn").toLowerCase()));
+            try {
+                assertEquals(testRun.getExecutedBy(), jsonObject.getString("executedBy"));
+            }catch(JSONException nE){
+                Logger.getAnonymousLogger().info("There is no EXECUTEDBY field in this test data["+i+"] bank.");
+            }
+                Iterator it = testRun.getDefects().iterator();
+            // Check defects array
+            int count = 0;
+            while (it.hasNext()) {
+                Defect myDefect = (Defect) it.next();
+                count++;
+                assertNotNull(myDefect.getKey());
+            }
+            assertEquals(count, jsonObject.getJSONArray("defects").length());
+            // Check evidences array
+            it = testRun.getEvidences().iterator();
+            count = 0;
+            while (it.hasNext()) {
+                Evidence evidence = (Evidence) it.next();
+                count++;
+                assertNotNull(evidence.getFileName());
+            }
+            assertEquals(count, jsonObject.getJSONArray("evidences").length());
+            // Check steps array
+            try {
+                it = testRun.getSteps().iterator();
+                count = 0;
+                while (it.hasNext()) {
+                    TestStep testStep = (TestStep) it.next();
+                    count++;
+                    assertNotNull(testStep.getStep());
+                }
+                assertEquals(count, jsonObject.getJSONArray("steps").length());
+            } catch (NullPointerException nE) {
+                // THERE IS NO STEPS ARRAY
+                Logger.getAnonymousLogger().info("There is no STEPS array in this test data["+i+"] bank.");
+            }
+            try {
+                // Check examples array (Cucumber)
+                it = testRun.getExamples().iterator();
+                count = 0;
+                while (it.hasNext()) {
+                    Example example = (Example) it.next();
+                    count++;
+                    assertNotNull(example.getValues());
+                }
+                assertEquals(count, jsonObject.getJSONArray("examples").length());
+            } catch (NullPointerException nE) {
+                // THERE IS NO EXAMPLES ARRAY
+                Logger.getAnonymousLogger().info("There is no EXAMPLES array in this test data["+i+"] bank.");
+            }
         }
-        assertEquals(count,jsonObject.getJSONArray("defects").length());
     }
 
 
